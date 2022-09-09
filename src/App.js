71f5170423366654b884328ca0f35c2d2650a893
PavlovskiiDmitrii <pavlovskii-777@mail.ru>
2022-09-09 11:47:49 +0300

    init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import 'antd/dist/antd.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  incrementAsync,
+  selectCount,
+} from './counterSlice';
+import { Button } from 'antd';
+import classNames from 'classnames';
+import './сounter.scss';
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div>
+        <span className={classNames('value')}>{count}</span>
+      </div>
+      <div>
+        <input
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <Button type="primary" onClick={() => dispatch(incrementAsync(incrementValue))}>Primary Button</Button>
+      </div>
+    </div>
+  );
+}
